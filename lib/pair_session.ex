91182cda8974a29ac392f8c230ae30f80eb8de8f
Suss Buzz <sussyoung9@gmail.com>
2021-05-05 09:33:06 +0545

    Update pair_session.ex

--- lib/pair_session.ex
+++ lib/pair_session.ex
@@ -1,9 +1,33 @@
 defmodule PairSession do
-  @spec count(String.t()) :: map()
-  def count(sentence) do
-    sentence
-    |> String.downcase()
-    |> String.split(~R/[^[:alnum:]\-]/u, trim: true)
-    |> Enum.reduce(Map.new(), fn word, map -> Map.update(map, word, 1, &(&1 + 1)) end)
+    @spec sum() :: integer()
+  def sum() do
+    0
+  end
+
+  @spec sum(String.t()) :: integer()
+  def sum(str) when is_binary(str) and byte_size(str) == 1 do
+    String.to_integer(str)
+  end
+
+  def sum("//" <> str) do
+    delimiter = str |> String.first()
+    sanitizer(str, delimiter)
+  end
+
+  def sum(str) do
+    sanitizer(str)
+  end
+
+  defp sanitizer(str, delimiter \\ ",") do
+    str
+    |> String.replace("\n", delimiter)
+    |> String.split(delimiter, trim: true)
+    |> sum_num
+  end
+
+  defp sum_num(list) do
+    list
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
   end
 end
